2958e86f11fe2370460e8abde88970616c70155d
lmiguelm26 <52018940+lmiguelm26@users.noreply.github.com>
2023-07-10 17:40:03 -0500

    Update canary.sh
    Ubuntu 20.04

--- canary.sh
+++ canary.sh
@@ -2,47 +2,24 @@
 ulimit -c unlimited
 while true; do 
 
-sudo apt update 
-sudo apt dist-upgrade
-sudo apt install git cmake build-essential autoconf libtool ca-certificates curl zip unzip tar pkg-config ninja-build ccache linux-headers-$(uname -r)
-sudo apt remove --purge cmake
-hash -r
-sudo apt install snapd
-sudo snap install cmake --classic
-cmake --version
-sudo add-apt-repository ppa:ubuntu-toolchain-r/test -y
-sudo apt update
-sudo apt install gcc-11 g++-11
-sudo update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-11 100 --slave /usr/bin/g++ g++ /usr/bin/g++-11 --slave /usr/bin/gcov gcov /usr/bin/gcov-11
-sudo update-alternatives --set gcc /usr/bin/gcc-11
-gcc-11 --version
-g++-11 --version
-sudo apt install acl
-setfacl -m u:www-data:rwx .
-cd ~
-git clone https://github.com/microsoft/vcpkg
-cd vcpkg
-./bootstrap-vcpkg.sh
-cd ~
-git clone --depth 1 https://github.com/opentibiabr/canary.git
-cd canary
-mv config.lua.dist config.lua
-mkdir build && cd build
-cmake -DCMAKE_TOOLCHAIN_FILE=~/vcpkg/scripts/buildsystems/vcpkg.cmake .. --preset linux-release
-cmake --build linux-release
-cd ~
-cd canary
-cp -r build/linux-release/bin/canary
 sudo apt update
 sudo apt install nginx
 sudo service nginx start
 sudo systemctl enable nginx
-sudo apt install ufw
+​sudo apt install ufw
 sudo ufw app list
-sudo ufw allow in "Nginx Full"
+sudo ufw allow in "Nginx Ful
+sudo apt install mariadb-server
+sudo service mariadb start
+sudo mysql -u root -p
+CREATE USER 'global'@'localhost' IDENTIFIED WITH mysql_native_password BY 'serverglobal1234';
+GRANT ALL PRIVILEGES ON *.* TO 'your_user'@'localhost' WITH GRANT OPTION; 
+exit
+sudo mysql_secure_installation
 sudo apt remove php*
 sudo apt update
 sudo apt upgrade
+sudo apt install python3-launchpadlib
 sudo apt install software-properties-common apt-transport-https -y
 sudo add-apt-repository ppa:ondrej/php -y
 sudo apt update
@@ -54,26 +31,78 @@ cd /
 cd etc
 sudo rm -r apache2
 sudo apt install phpmyadmin
-sudo apt install mysql-server
-sudo service mysql start
-sudo apt install mysql-serversudo service mysql start
-cd /var/www/html
-sudo wget -c https://github.com/opentibiabr/myaac/archive/refs/heads/main.zip -O myaac-global.zip
-sudo unzip myaac-global.zip
-sudo mv 'MyAAC Global'/* /var/www/html
-sudo rm -rf myaac-global.zip 'MyAAC Global'
-sudo rm index.html
-sudo cp config.php config.local.php
-sudo chown -R www-data.www-data /var/www/html
-sudo chmod 660 config.php 
-sudo chmod 660 config.local.php
-sudo chmod 660 images/guilds
-sudo chmod 660 images/houses
-sudo chmod 660 images/gallery
-sudo chmod -R 770 system/cache
-sudo mysql -u root -p
-CREATE USER 'global'@'localhost' IDENTIFIED WITH mysql_native_password BY '6oSN83Tp8AGpP6n';
-GRANT ALL PRIVILEGES ON *.* TO 'global'@'localhost' WITH GRANT OPTION; 
+sudo ln -s /usr/share/phpmyadmin /var/www/html/phpmyadmin
+# Contenido de reemplazo
+contenido_reemplazo=$(cat <<EOL
+server {
+    listen       80;
+    listen       [::]:80;
+    #   listen       443 ssl http2;
+    #   listen       [::]:443 ssl http2;
+
+    server_name _;
+    #   server_name example.com  www.example.com;
+
+    root   /var/www/html/;
+    index index.php index.html index.htm;
+
+    #   if ($host !~ ^(exemple.com|www.example.com)$){
+    #       return 400;
+    #   }
+
+    if (\$request_method !~ ^(GET|HEAD|POST)$) {
+        return 444;
+    }
+
+    location ~ /system {
+        deny all;
+        return 404;
+    }
+
+    location ~ /\.ht {
+        deny all;
+    }
+
+    location ~ /\.git {
+        return 403;
+    }
+
+    location ~* \.(pl|cgi|py|sh|lua)\$ {
+        return 403;
+    }
+
+    location ~* ^/wp-content/uploads/.*\.(asp|cgi|htm|html|js|jsp|php|pl|py|sh|shtml|swf)$ {
+        return 403;
+    }
+
+    location ~* ^/(readme|license|schema|password|passwords).*\.(txt|html)$ {
+        return 403;
+    }
+
+    location / {
+        try_files \$uri \$uri/ /index.php?\$query_string;
+        #       try_files \$uri \$uri/ /index.php;
+    }
+
+    location ~ ^/(conf|doc|sql|setup)/ {
+        deny all;
+        return 403;
+    }
+
+    #   Pass PHP Scripts To FastCGI Server
+    location ~ \.php$ {
+        #       root /var/www/html/;
+        fastcgi_pass unix:/run/php/php7.4-fpm.sock;
+        fastcgi_index index.php;
+        fastcgi_param SCRIPT_FILENAME \$document_root\$fastcgi_script_name;
+        include fastcgi_params;
+    }
+}
+EOL
+)
+sudo tee "$archivo" <<< "$contenido_reemplazo" > /dev/null
+sudo service nginx restart
+
 exit
 
 done
